4dcf38bd47e512eec47f67ec48053efea0aebf92
Akash Deep <2001akashdeep@gmail.com>
2022-09-04 13:48:05 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+
+// Your web app's Firebase configuration
+let firebaseConfig = {
+    apiKey: "AIzaSyDCIJ1EXrWieDh8zVPgjlR7RRxE5bPhRwI",
+    authDomain: "blog-website-47189.firebaseapp.com",
+    projectId: "blog-website-47189",
+    storageBucket: "blog-website-47189.appspot.com",
+    messagingSenderId: "200507747911",
+    appId: "1:200507747911:web:840fdda45ba571134e4b62"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
